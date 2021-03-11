c62e849be3f4f831041adf286539fcc556e0f95d
uladzislau.maskoukin <Uladzislau_Maskoukin@epam.com>
2021-03-11 21:45:06 +0300

    10master

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("MainMaster1");
+        System.out.println("MainMaster2");
     }
 
     public void read() {}
